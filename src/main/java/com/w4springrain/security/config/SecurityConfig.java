e717032bf76cbcd319ccc5b8e99e81a1708c74d2
moregorenine <moregorenine@gmail.com>
2020-07-10 00:15:10 +0900

    사용자 정의 보안 기능 구현

---
+++ src/main/java/com/w4springrain/security/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.w4springrain.security.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+        http
+                .formLogin();
+    }
+}

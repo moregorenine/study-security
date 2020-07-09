6feebef72ffc3cc5da1b2dd3d664716899f43b20
moregorenine <moregorenine@gmail.com>
2020-07-09 23:40:10 +0900

    Init Project

---
+++ src/main/java/com/w4springrain/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.w4springrain.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
